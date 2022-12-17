cc91921386d166fbf3ecc962e0aca8c11040073e
KOSASIH <kosasihg88@gmail.com>
2022-12-17 15:11:37 +0700

    Add files via upload

---
+++ app (9).py
@@ -0,0 +1,29 @@
+from sagemaker.huggingface import HuggingFaceModel
+import boto3
+
+iam_client = boto3.client('iam')
+role = iam_client.get_role(RoleName='{IAM_ROLE_WITH_SAGEMAKER_PERMISSIONS}')['Role']['Arn']
+# Hub Model configuration. https://huggingface.co/models
+hub = {
+	'HF_MODEL_ID':'cardiffnlp/twitter-roberta-base-sentiment',
+	'HF_TASK':'text-classification'
+}
+
+# create Hugging Face Model Class
+huggingface_model = HuggingFaceModel(
+	transformers_version='4.17.0',
+	pytorch_version='1.10.2',
+	py_version='py38',
+	env=hub,
+	role=role, 
+)
+
+# deploy model to SageMaker Inference
+predictor = huggingface_model.deploy(
+	initial_instance_count=1, # number of instances
+	instance_type='ml.m5.xlarge' # ec2 instance type
+)
+
+predictor.predict({
+	'inputs': "I like you. I love you"
+})
